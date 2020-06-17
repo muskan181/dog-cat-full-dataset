335d724d8b34856dac7fcb0f000df132f433bf16
muskan181 <60434036+muskan181@users.noreply.github.com>
2020-06-17 12:10:22 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,7 +1,7 @@
 import smtplib
 session = smtplib.SMTP("smtp.gmail.com", 587)
 session.starttls()
-session.login("muskan.181.ma@gmail.com","march0303")
+session.login("muskan.181.ma@gmail.com","abcd")
 msg = "Best model trained!"
 session.sendmail("muskan.181.ma@gmail.com" , "muskan.181.ma@gmail.com" , msg)
 session.quit()
